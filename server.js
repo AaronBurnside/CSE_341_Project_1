888950a6e8cb0968647b756ccdb754801cef0495
AaronBurnside <aburns29@byui.edu>
2023-09-16 19:29:15 -0600

    mongo setup for project 1-3

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config()
+const connectionString = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}localhost:27017`;
 const app = express();
 
 const port = process.env.PORT || 3000;
